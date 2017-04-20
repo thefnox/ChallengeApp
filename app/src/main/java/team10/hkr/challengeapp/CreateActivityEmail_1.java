756a8e8552abae7244a61449b3ec4405ca4b34dd
Martin <m.huynh@hotmail.co.uk>
2017-04-20 17:04:04 +0200

    Second Test commit - EmailActivity

---
+++ app/src/main/java/team10/hkr/challengeapp/CreateActivityEmail_1.java
@@ -0,0 +1,13 @@
+package team10.hkr.challengeapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CreateActivityEmail_1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_email_1);
+    }
+}
