3fe48f938c023f3761de719e2a66e8d4f95baa3d
Martin <m.huynh@hotmail.co.uk>
2017-04-20 20:57:42 +0200

    Create account views email, fullname/password/username, add profile photo

---
+++ app/src/main/java/team10/hkr/challengeapp/CreateActivity_1_Email.java
@@ -0,0 +1,13 @@
+package team10.hkr.challengeapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CreateActivity_1_Email extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_1_email);
+    }
+}
---
+++ app/src/main/java/team10/hkr/challengeapp/CreateActivity_2_FullNamePassword.java
@@ -0,0 +1,13 @@
+package team10.hkr.challengeapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CreateActivity_2_FullNamePassword extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_2__full_name_password);
+    }
+}
