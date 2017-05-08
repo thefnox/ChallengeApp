6e8d813c96d3a2308b39422a767b937fbf1b4bd2
CharlieTunasson <charlietunasson@hotmail.com>
2017-05-08 21:48:42 +0200

    simple start for the search activity
    appbar needs to be removed and the listView needs to be added once the
    database wiring is done.

---
+++ app/src/main/java/team10/hkr/challengeapp/Controllers/SearchActivity.java
@@ -0,0 +1,15 @@
+package team10.hkr.challengeapp.Controllers;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import team10.hkr.challengeapp.R;
+
+public class SearchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+    }
+}
