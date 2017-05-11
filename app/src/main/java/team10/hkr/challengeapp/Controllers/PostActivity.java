a8bfc834fe02ef7b171b94fad2fc861a9e85a651
CharlieTunasson <charlietunasson@hotmail.com>
2017-05-12 01:01:54 +0200

    wiring started for the postActivity

--- app/src/main/java/team10/hkr/challengeapp/Controllers/PostActivity.java
+++ app/src/main/java/team10/hkr/challengeapp/Controllers/PostActivity.java
@@ -1,8 +1,10 @@
 package team10.hkr.challengeapp.Controllers;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,6 +15,24 @@ import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import team10.hkr.challengeapp.Models.Post;
 import team10.hkr.challengeapp.R;
 
 import static android.R.attr.id;
@@ -23,6 +43,22 @@ public class PostActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post);
+
+        final String url = "http://95.85.16.177:3000/api/post";
+        final JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                Log.v("YUPPI", "Response; " + response.toString());
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.v("WTF", "Err: " + error.getLocalizedMessage());
+            }
+        });
+
+        Volley.newRequestQueue(this).add(jsonObjectRequest);
+
 
         String[] comments = {                                                                                                                   //
                 "Hello there!",                                                                                                                 //
@@ -52,8 +88,8 @@ public class PostActivity extends AppCompatActivity {
 
     public void onReportClick(View view) {
 
-            Intent reportIntent = new Intent(this, ReportActivity.class);
-            startActivity(reportIntent);
+        Intent reportIntent = new Intent(this, ReportActivity.class);
+        startActivity(reportIntent);
 
     }
 }
