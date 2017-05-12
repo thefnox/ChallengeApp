ee95eb9ae28edd682b02b390a1fc330c52e548a4
CharlieTunasson <charlietunasson@hotmail.com>
2017-05-12 20:24:29 +0200

    testing singin-

--- app/src/main/java/team10/hkr/challengeapp/Controllers/ProfileActivity.java
+++ app/src/main/java/team10/hkr/challengeapp/Controllers/ProfileActivity.java
@@ -2,17 +2,29 @@ package team10.hkr.challengeapp.Controllers;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import team10.hkr.challengeapp.Models.Pokemon;
 import team10.hkr.challengeapp.Models.User;
 import team10.hkr.challengeapp.R;
 
 public class ProfileActivity extends AppCompatActivity {
 
 
-
+    User user;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,7 +37,7 @@ public class ProfileActivity extends AppCompatActivity {
     private void fillProfileInfo(){
 
         ImageView profilePicture = (ImageView) findViewById(R.id.profilePicture);
-        TextView profileName = (TextView) findViewById(R.id.profileName);
+        final TextView profileName = (TextView) findViewById(R.id.profileName);
         TextView profileCity = (TextView) findViewById(R.id.profileCity);
         TextView profileCountry = (TextView) findViewById(R.id.profileCountry);
         TextView profileStars = (TextView) findViewById(R.id.profileStars);
@@ -35,18 +47,32 @@ public class ProfileActivity extends AppCompatActivity {
         TextView profileDescription = (TextView) findViewById(R.id.profileDescription);
 
 
+        final String url = "http://95.85.16.177:3000/api/user/me";
+        final JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                Log.v("YUPPI", "Response; " + response.toString());
+                try {
+                    user = new User(response);
+                    Toast.makeText(ProfileActivity.this, response.getString("name"), Toast.LENGTH_LONG).show();
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.v("WTF", "Err: " + error.getLocalizedMessage());
+            }
+        });
+
+        Volley.newRequestQueue(this).add(jsonObjectRequest);
+
         profilePicture.setImageResource(R.drawable.profile_picture_test);
-
-//        profileName.setText(user.getRealName());
-//        profileStars.setText(String.valueOf(user.getStars())+ getString(R.string.stars));
-//        profileCity.setText(user.getCity());
-//        profileCountry.setText(user.getCountry());
-//        profileChampions.setText(String.valueOf(user.getChampions())+ getString(R.string.champions));
-//        profileFacebook.setText(user.getFacebookLink());
-//        profileTwitter.setText(user.getTwitterHandle());
-//        profileDescription.setText(user.getProfileDescription());
-
-
+        if( user != null) {
+            profileName.setText(user.getCity());
+        }
     }
 
 
