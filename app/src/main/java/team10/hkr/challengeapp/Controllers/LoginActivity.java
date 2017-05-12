8679c92d51277ba0cded678ac6842f700bb0598c
Martin <martin.huynh0055@stud.hkr.se>
2017-05-12 19:18:24 +0200

    Changed manifest to load sign in page

--- app/src/main/java/team10/hkr/challengeapp/Controllers/LoginActivity.java
+++ app/src/main/java/team10/hkr/challengeapp/Controllers/LoginActivity.java
@@ -3,8 +3,19 @@ package team10.hkr.challengeapp.Controllers;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
@@ -13,6 +24,13 @@ import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import team10.hkr.challengeapp.Models.Pokemon;
 import team10.hkr.challengeapp.R;
 import team10.hkr.challengeapp.Controllers.SignUpActivity.SignUpActivity1Email;
 
@@ -20,6 +38,9 @@ public class LoginActivity extends AppCompatActivity {
 
     LoginButton facebook_login_button;
     CallbackManager callbackManager;
+    EditText usernameOrEmail;
+    EditText password;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,6 +61,7 @@ public class LoginActivity extends AppCompatActivity {
         callbackManager = CallbackManager.Factory.create();
         facebook_login_button = (LoginButton)findViewById(R.id.facebook_login_button);
     }
+
 
     private void loginWithFB() {
         LoginManager.getInstance().registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
