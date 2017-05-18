f13d5610d0cc0e16de3c219e3f5260bfdd3933f8
Martin <martin.huynh0055@stud.hkr.se>
2017-05-18 19:45:07 +0200

    Users can now delete their posts (although it's a bit buggy)

--- app/src/main/java/team10/hkr/challengeapp/PostRecyclerAdapter.java
+++ app/src/main/java/team10/hkr/challengeapp/PostRecyclerAdapter.java
@@ -2,11 +2,13 @@ package team10.hkr.challengeapp;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.StrictMode;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -16,7 +18,13 @@ import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.MediaController;
 import android.widget.TextView;
+import android.widget.Toast;
 import android.widget.VideoView;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
 
 import org.json.JSONException;
 
@@ -27,8 +35,11 @@ import java.util.List;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 import team10.hkr.challengeapp.Controllers.CommentActivity;
+import team10.hkr.challengeapp.Controllers.LoginActivity;
+import team10.hkr.challengeapp.Controllers.PrimaryActivity;
 import team10.hkr.challengeapp.Controllers.ProfileActivity;
 import team10.hkr.challengeapp.Controllers.ReportActivity;
+import team10.hkr.challengeapp.Controllers.SettingsActivity.CloseAccountActivity;
 import team10.hkr.challengeapp.Models.Post;
 
 /**
@@ -58,12 +69,51 @@ public class PostRecyclerAdapter extends RecyclerView.Adapter<PostRecyclerAdapte
 
         holder.profilePhotoView.setImageResource(R.drawable.com_facebook_profile_picture_blank_portrait);
         holder.userNameTextView.setText("SakuraChan");
+
         if(holder.itemView.getContext().getClass() == ProfileActivity.class) {
+            final Activity activity = (Activity) holder.itemView.getContext();
             holder.deleteButtonImageButton.setVisibility(View.VISIBLE);
             holder.deleteButtonImageButton.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    //To do
+
+                    new AlertDialog.Builder(activity)
+
+                            .setTitle("Close Account")
+                            .setMessage("Are you sure you want to delete this post?")
+                            .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int which) {
+
+                                    final String URL = "http://95.85.16.177:3000/api/post/"+post.getUUID();
+
+                                    StringRequest stringRequest = new StringRequest(Request.Method.DELETE, URL, new Response.Listener<String>() {
+                                        @Override
+                                        public void onResponse(String response) {
+                                            Toast.makeText(activity,"Post Deleted", Toast.LENGTH_LONG).show();
+                                            Intent intent = new Intent(activity, PrimaryActivity.class);
+                                            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                                            activity.startActivity(intent);
+
+                                        }
+                                    }, new Response.ErrorListener() {
+                                        @Override
+                                        public void onErrorResponse(VolleyError error) {
+                                            Toast.makeText(activity, "Something went wrong", Toast.LENGTH_LONG).show();
+                                        }
+                                    });
+
+                                    RequestQueueSingleton.getInstance(activity).addToRequestQueue(stringRequest);
+
+
+                                }
+                            })
+                            .setNegativeButton(android.R.string.no, new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int which) {
+
+                                }
+                            })
+                            .setIcon(android.R.drawable.ic_dialog_alert)
+                            .show();
                 }
             });
         }
@@ -188,4 +238,5 @@ public class PostRecyclerAdapter extends RecyclerView.Adapter<PostRecyclerAdapte
         }
         return false;
     }
+
 }
